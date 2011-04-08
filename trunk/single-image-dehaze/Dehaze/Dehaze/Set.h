25c172e3da34516bc3e8332bb05c7ffceb22022e
whuyinpeng@gmail.com <whuyinpeng@gmail.com@2838f9b1-1ba2-c7a5-2f1c-945fbee36071>
2011-04-08 05:25:26 +0000

    Added a missing header file!

---
+++ trunk/single-image-dehaze/Dehaze/Dehaze/Set.h
@@ -0,0 +1,18 @@
+#ifndef SET_H
+#define SET_H
+
+#define OMG 0.95
+#define AIR 0.99
+#define P   0.95
+
+#define DD 20
+#define DRC 10
+#define DRL 10
+
+#define BDD 20
+#define BDR 20
+
+#define BIN 16
+#define BBIN 8
+
+#endif
